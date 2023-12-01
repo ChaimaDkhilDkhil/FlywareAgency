cea2795b6f76320e4df975e9f6c90815814cbf7e
mariem benyoussef <benyoussefmariem@bizerte.r-iset.tn>
2023-12-01 18:56:20 +0100

    transport2

--- app/src/main/java/com/example/dashboard/transportBookingFragment.kt
+++ app/src/main/java/com/example/dashboard/transportBookingFragment.kt
@@ -1,6 +1,9 @@
 package com.example.dashboard
 
+import android.net.http.HttpException
+import android.os.Build
 import android.os.Bundle
+import android.os.ext.SdkExtensions
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,17 +12,14 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import retrofit2.HttpException
 import java.io.IOException
 
 class transportBookingFragment : Fragment() {
-
     private lateinit var recyclerView: RecyclerView
     private var mList = ArrayList<TransportBooking>()
     private lateinit var manager: RecyclerView.LayoutManager
@@ -38,51 +38,55 @@ class transportBookingFragment : Fragment() {
     @OptIn(DelicateCoroutinesApi::class)
     private fun getRequest() {
         GlobalScope.launch(Dispatchers.IO) {
-            try {
-                val response = RetrofitInstance.apit.getTransportBookings()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && SdkExtensions.getExtensionVersion(
+                    Build.VERSION_CODES.S) >= 7) {
+                try {
+                    val response = RetrofitInstance.apit.getTransportBookings()
 
-                if (response.isSuccessful && response.body() != null) {
-                    withContext(Dispatchers.Main) {
-                        val transportBookings = response.body()
-                        if (transportBookings != null) {
-                            for (transportBooking in transportBookings) {
-                                val transportItem = TransportBooking(
-                                    transportBooking.id,
-                                    transportBooking.name ?: "",
-                                    transportBooking.pays ?: "",
-                                    transportBooking.title ?: "",
-                                    transportBooking.location ?: "",
-                                    transportBooking.price ?: "",
-                                    transportBooking.description ?: "",
-                                    transportBooking.nbPersonne ?: 0,
-                                    transportBooking.date ?: "",
-                                    transportBooking.luggage ?: 0
-                                )
-                                mList.add(transportItem)
-                            }
+                    if (response.isSuccessful && response.body() != null) {
+                        withContext(Dispatchers.Main) {
+                            val transportBookings = response.body()
+                            if (transportBookings != null) {
+                                for (transportBooking in transportBookings) {
+                                    val transportItem = TransportBooking(
+                                        transportBooking.id,
+                                        transportBooking.name ?: "",
+                                        transportBooking.pays ?: "",
+                                        transportBooking.title ?: "",
+                                        transportBooking.location ?: "",
+                                        transportBooking.price ?: "",
+                                        transportBooking.description ?: "",
+                                        transportBooking.nbPersonne ?: 0,
+                                        transportBooking.date ?: "",
+                                        transportBooking.luggage ?: 0
+                                    )
+                                    mList.add(transportItem)
+                                }
 
-                            withContext(Dispatchers.Main) {
-                                myAdapter = TransportBookingAdapter(mList)
-                                myAdapter.notifyDataSetChanged()
+                                withContext(Dispatchers.Main) {
+                                    myAdapter = TransportBookingAdapter(mList)
+                                    myAdapter.notifyDataSetChanged()
 
-                                manager = LinearLayoutManager(requireContext())
-                                recyclerView.layoutManager = manager
-                                recyclerView.adapter = myAdapter
+                                    manager = LinearLayoutManager(requireContext())
+                                    recyclerView.layoutManager = manager
+                                    recyclerView.adapter = myAdapter
+                                }
                             }
                         }
                     }
-                }
-            } catch (e: HttpException) {
-                withContext(Dispatchers.Main) {
-                    Toast.makeText(requireContext(), "HTTP error ${e.message}", Toast.LENGTH_LONG).show()
-                    Log.e("transportError", e.toString())
-                }
-            } catch (e: IOException) {
-                withContext(Dispatchers.Main) {
-                    Toast.makeText(requireContext(), "App error ${e.message}", Toast.LENGTH_LONG).show()
-                    Log.e("transportError", e.toString())
+                } catch (e: HttpException) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(requireContext(), "HTTP error ${e.message}", Toast.LENGTH_LONG).show()
+                        Log.e("transportError", e.toString())
+                    }
+                } catch (e: IOException) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(requireContext(), "App error ${e.message}", Toast.LENGTH_LONG).show()
+                        Log.e("transportError", e.toString())
+                    }
                 }
             }
         }
     }
+
 }
